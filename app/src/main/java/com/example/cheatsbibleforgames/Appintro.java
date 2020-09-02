48ee1cca79461272a9772cea5b482c2d0209a900
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2020-09-02 21:19:24 +0530

    sid #2 commit

--- app/src/main/java/com/example/cheatsbibleforgames/Appintro.java
+++ app/src/main/java/com/example/cheatsbibleforgames/Appintro.java
@@ -1,14 +1,77 @@
 package com.example.cheatsbibleforgames;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-public class Appintro extends AppCompatActivity {
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class Appintro extends AppIntro {
+
+
+    SharedPreferences mSharedPreferences;
+    SharedPreferences.Editor sharedprefeditor;
+
+    public void transtion_main_activity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appintro);
+        //setContentView(R.layout.activity_appintro);
+
+       // getSupportActionBar().hide();
+
+        addSlide(AppIntroFragment.newInstance("Welcome to the app","first slide",R.drawable.firstcropped,
+                ContextCompat.getColor(getApplicationContext(),R.color.grey)));
+
+
+
+        mSharedPreferences  = getApplicationContext().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+
+        sharedprefeditor  = mSharedPreferences.edit();
+
+        if(mSharedPreferences !=null){
+            boolean checkshared = mSharedPreferences.getBoolean("checkstate",false);
+
+            if(checkshared == true){
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        }
+
+    }
+
+
+    @Override
+    public void  onSkipPressed(Fragment currentFragment) {
+
+        super.onSkipPressed(currentFragment);
+            // Decide what to do when the user clicks on "Skip"
+            transtion_main_activity();
+            sharedprefeditor.putBoolean("checkstate",false).commit();
+
+
+            finish();
+    }
+
+    @Override
+        public void  onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Decide what to do when the user clicks on "Done"
+        transtion_main_activity();
+        sharedprefeditor.putBoolean("checkstate",true).commit();
+        finish();
     }
 }
--- app/src/main/java/com/example/cheatsbibleforgames/MainActivity.java
+++ app/src/main/java/com/example/cheatsbibleforgames/MainActivity.java
@@ -2,14 +2,44 @@ package com.example.cheatsbibleforgames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgpubg;
+    private ImageView vicecity;
+    private ImageView imgsanandreas;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgpubg = (ImageView) findViewById(R.id.imgpubg);
+        imgsanandreas = (ImageView) findViewById(R.id.imgsanandreas);
+        vicecity = (ImageView) findViewById(R.id.imgvicecity);
+
+        imgsanandreas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sanandreas();
+            }
+        });
     }
+
+    public void sanandreas(){
+        Intent intentsandreas = new Intent(this,Sanandreas.class);
+    }
+
+
+
+
+
+
+
 
 }
