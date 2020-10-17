506d028ae7183f4a74a7ce21c664e3c22afee89a
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2020-10-17 15:03:24 +0530

    sid #17 commit

--- app/src/main/java/com/cheatsdev/cheatsbibleforgames/Farcry3.java
+++ app/src/main/java/com/cheatsdev/cheatsbibleforgames/Farcry3.java
@@ -1,22 +1,20 @@
 package com.cheatsdev.cheatsbibleforgames;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Bioshock extends AppCompatActivity {
+public class Farcry3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bioshock);
+        setContentView(R.layout.activity_farcrythree);
 
         // git master
         //
-
-
-
-
 
     }
 }
